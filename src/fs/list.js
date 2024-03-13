a5e352c514bd6093b6001ae62c5003219047b15e
Regsey46 <gregory-77@mail.ru>
2024-03-13 18:55:53 +0300

    List

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { existsSync } from 'fs';
+import { readdir } from 'fs/promises';
+import { join } from 'path'
+
+const dirPath = join(import.meta.dirname, 'files')
+
 const list = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(dirPath)) throw new Error('FS operation failed')
+
+    const files = await readdir(dirPath)
+    console.log(files)
+  } catch (e) {
+    console.error(e.message)
+  }
 };
 
 await list();
