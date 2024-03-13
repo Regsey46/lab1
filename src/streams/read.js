28a9422c78c11d99b9a06704066e9ab999094fb5
Regsey46 <gregory-77@mail.ru>
2024-03-13 19:03:55 +0300

    streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from 'fs';
+import { join } from 'path'
+
+const fileToReadPath = join(import.meta.dirname, 'files', 'fileToRead.txt')
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(fileToReadPath)
+  stream.on('data', (chunk) => process.stdout.write(chunk))
 };
 
 await read();
