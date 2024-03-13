1316787a319111cb1894792a901852c438358043
Regsey46 <gregory-77@mail.ru>
2024-03-13 19:00:33 +0300

    Args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const rawArgs = process.argv.slice(2)
+    const args = rawArgs.join(' ').matchAll(/--(\S+)\s+([^ ]+)/g)
+    
+    for (const rawFlag of args) {
+        const [, flag, value] = rawFlag
+        console.log(`${flag} is ${value}`)
+    }
 };
 
 parseArgs();
