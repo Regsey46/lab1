9a73340d3ea54fe08475fa36c4824ae0c25f80c6
Regsey46 <gregory-77@mail.ru>
2024-03-13 18:54:49 +0300

    Delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { existsSync } from 'fs'
+import { unlink } from 'fs/promises'
+import { join } from 'path'
+
+const filePath = join(import.meta.dirname, 'files', 'fileToRemove.txt')
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(filePath)) throw new Error('FS operation failed')
+
+    await unlink(filePath)
+  } catch (e) {
+    console.error(e.message)
+  }
 };
 
 await remove();
